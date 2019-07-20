5f9876bcd69c52053ff875d2ae43ee5ec3464b51
fabriond <gfbf@ic.ufal.br>
2019-07-20 01:38:20 -0300

    Changed to newest vue cli build

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <div class="navbar">
+      <a href="/" class="left">Gossiper</a>
+      <div>O site para comentar nas despesas públicas do portal da transparência</div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+}
+body{margin: 0;}
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 500;
+}
+.navbar a, .navbar div{
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+.navbar a {
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 30px;
+  background-color: #42b983;
+}
+.navbar div {
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 30px;
+}
+.right{
+  float: right;
+}
+.left{
+  float: left;
+}
+.navbar a:hover {
+  background: #ddd;
+  color: black;
+}
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,362 @@
+<template>
+  <div class="hello">
+    <br><br><br>
+    <div v-if="!docTransparencia"><br>
+      <a class="tutorial" target="_blank"
+        href="http://www.portaltransparencia.gov.br/despesas/favorecido?ordenarPor=valor&direcao=desc">Procure aqui códigos de despesas</a>
+      <p class="tutorial" >
+        Para achar o código da despesa, basta clicar em qualquer <br>
+        documento e pegar a última parte da url antes da interrogação
+      </p>
+      <img v-if="!docTransparencia" src="../assets/logo.gif">
+    </div>
+    <label for="codigo">Código da despesa</label>
+    <input id="codigo" class="group" type="text" v-on:input="codigo = $event.target.value">
+    <button :disabled="loading" v-on:click="getSpecificDocument(), loading=true">Procurar despesa pública</button>
+    <p></p>
+    <body>
+      <table align="center" v-if="docTransparencia">
+        <thead>
+          <tr v-bind:key="topic" v-for="(value, topic) in (Object.keys(docTransparencia), docTransparencia)">
+            <div>
+              <th>{{removeCamelCase(topic)}}</th>
+              <td>
+                <div v-if="topic == 'valor'">R$ {{trim(value)}}</div>
+                <div v-else>{{trim(value)}}</div>
+              </td>
+            </div>
+          </tr>
+        </thead>
+      </table>
+      <div class="row">
+        <div class="column" v-bind:key="comment" v-for="comment in comments">
+          <div class="card">
+            <div v-if="edit_id != comment._id">
+              <h3>{{comment.author}}</h3>
+              <p>{{comment.comment}}</p>
+              <button id="delete" class="round-button" v-on:click="deleteComment(comment._id)"><img id="menu" src="../assets/delete.png"></button>
+              <button id="edit" class="round-button" v-on:click="edit_id = comment._id"><img id="menu" src="../assets/edit.png"></button>
+            </div>
+            <div v-if="edit_id == comment._id">
+              <input id="author" type="text" :placeholder="comment.author" v-on:input="edit_auth = $event.target.value">
+              <input id="comment" type="text" :placeholder="comment.comment" v-on:input="edit_com = $event.target.value">
+              <button id="save" class="submit" v-on:click="editComment(comment._id, edit_auth, edit_com, comment)">Salvar</button>
+              <button id="cancel" class="submit" v-on:click="edit_id = ''">Cancelar</button>
+            </div>
+          </div>
+        </div>
+        <div class="column" v-if="docTransparencia">
+          <div class="card">
+            <input id="author" type="text" placeholder="Autor" v-on:input="auth = $event.target.value">
+            <input id="comment" type="text" placeholder="Comentário" v-on:input="com = $event.target.value">
+            <button id="create" class="submit" v-on:click="postComment(auth, com)">Comentar</button>
+          </div>
+        </div>
+      </div>
+    </body>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'MainPage',
+  data () {
+    return {
+      codigo: '',
+      docTransparencia: '',
+      comments: '',
+      auth: '',
+      com: '',
+      edit_auth: '',
+      edit_com: '',
+      loading: false,
+      edit_id: ''
+    }
+  },
+
+  methods: {
+    getSpecificDocument: function () {
+      axios.get(process.env.VUE_APP_API + this.codigo + '/comments').then((response) => {
+        this.docTransparencia = ''
+        this.comments = ''
+        this.docTransparencia = response.data
+        this.comments = this.docTransparencia.comments
+        delete this.docTransparencia.comments
+        this.loading = false
+        let toast = this.$toasted.show('Documento encontrado', {
+          theme: 'toasted-primary',
+          position: 'top-left',
+          duration: 3000
+        })
+        toast.el.style.fontFamily = "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif"
+      }).catch(() => {
+        this.docTransparencia = ''
+        this.comments = ''
+        this.loading = false
+        let toast = this.$toasted.show('Documento não encontrado', {
+          theme: 'toasted-primary',
+          position: 'top-left',
+          duration: 3000
+        })
+        toast.el.style.fontFamily = "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif"
+      })
+    },
+
+    postComment: function (author, comment) {
+      axios.post(process.env.VUE_APP_API + this.codigo + '/comments', {author: author, comment: comment}).then((response) => {
+        this.comments.push(response.data)
+
+        document.getElementById('author').value = ''
+        document.getElementById('comment').value = ''
+        this.auth = ''
+        this.com = ''
+      })
+    },
+
+    editComment: function (commentId, author, comment, prevComment) {
+      if (comment == null || comment === '') comment = prevComment.comment
+      if (author == null || author === '') author = prevComment.author
+
+      axios.put(process.env.VUE_APP_API + this.codigo + '/comments/' + commentId, {author: author, comment: comment}).then((response) => {
+        const editIndex = this.comments.findIndex((element) => {
+          return element._id === prevComment._id
+        })
+
+        this.comments[editIndex].author = response.data.author
+        this.comments[editIndex].comment = response.data.comment
+
+        this.edit_id = ''
+        this.edit_auth = ''
+        this.edit_com = ''
+      })
+    },
+
+    deleteComment: function (commentId) {
+      axios.delete(process.env.VUE_APP_API + this.codigo + '/comments/' + commentId).then((response) => {
+        var aux = this.comments
+        const deleteIndex = aux.findIndex((element) => {
+          return element._id === response.data._id
+        })
+
+        aux.splice(deleteIndex, 1)
+        this.comments = aux
+      })
+    },
+
+    removeCamelCase: function (text) {
+      var result = text.replace(/([A-Z])/g, ' $1')
+      return result.charAt(0).toUpperCase() + result.slice(1)
+    },
+
+    trim: function (text) {
+      if (text != null) return text.toString().trim()
+      else return null
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+.tutorial{
+  font-size: 24px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+#menu{
+  width: 20px;
+  border-radius: 20%;
+  float:left;
+}
+button {
+  vertical-align: 20px;
+  padding: 15px 25px;
+  font-size: 24px;
+  text-align: center;
+  cursor: pointer;
+  outline: none;
+  color: #fff;
+  background-color: #42b983;
+  border: none;
+  border-radius: 15px;
+  box-shadow: 0 9px #389b6e;
+}
+button:hover {
+  background-color: #41b681;
+}
+button:active {
+  background-color: #41b681;
+  box-shadow: 0 5px #389b6e;
+  transform: translateY(4px);
+}
+button:disabled {
+  background-color: #699;
+  box-shadow: 0 5px #999;
+  transform: translateY(4px);
+  cursor: default;
+}
+table {
+  border: 5px solid #42b983;
+  border-radius: 3px;
+  background-color: #fff;
+  font-size: 16px;
+  display: inline-block;
+}
+th {
+  background-color: #42b983;
+  color: rgba(255,255,255,0.66);
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+td {
+  background-color: #f9f9f9;
+}
+th, td {
+  min-width: 170px;
+  padding: 10px 15px;
+  text-align: left;
+}
+th, td, button, h1, h2, #tutorial, label{
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+input{
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  width: 60%;
+  max-width: 270px;
+  margin: 0px auto;
+  padding: 10px;
+  position: relative;
+  overflow: hidden;
+  font-size: 20px;
+}
+input#codigo{
+  margin: 10px auto;
+  width: 30%;
+}
+input:focus{
+  border-color: #7185ec;
+}
+input, label{
+  display: block;
+}
+#create, #save, #cancel{
+  width: 30%;
+  padding: 6px;
+}
+#author, #comment{
+  text-align: center;
+}
+label{
+  font-size: 20px;
+  margin-top: 10px;
+  margin-left: -140px;
+}
+.submit{
+  margin-top: 5px;
+  margin-bottom: 5px;
+  font-size: 20px;
+}
+/* Float four columns side by side */
+.column {
+  display:inline-block;
+  width: 25%;
+  padding: 0 10px;
+}
+
+/* Remove extra left and right margins, due to padding */
+.row {
+  margin-top: 10px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive columns */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
+  }
+}
+
+/* Style the counter cards */
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 16px;
+  text-align: center;
+  background-color: #f1f1f1;
+  border: 5px solid #42b983;
+  margin-bottom: 10px;
+  overflow-wrap: break-word;
+}
+
+.round-button {
+  float: right;
+  padding: 16px;
+  margin-top: -50px;
+}
+.round-button:active {
+  transform: translateY(4px);
+}
+.round-button:disabled {
+  transform: translateY(4px);
+  cursor: default;
+}
+#delete{
+  background-color: #b82727; /* Red */
+  box-shadow: 0 7px #8a2222;
+}
+#delete:hover{
+  background-color: #aa2f2f;
+}
+#delete:active{
+  background-color: #aa2f2f;
+  box-shadow: 0 5px #8a2222;
+}
+#delete:disabled{
+  background-color: #955;
+  box-shadow: 0 5px #999;
+}
+
+#edit{
+  margin-right: 10px;
+  background-color: #42b983; /* Red */
+  box-shadow: 0 7px #389b6e;
+}
+#edit:hover{
+  background-color: #41b681;
+}
+#edit:active{
+  background-color: #41b681;
+  box-shadow: 0 5px #389b6e;
+}
+#edit:disabled{
+  background-color: #699;
+  box-shadow: 0 5px #999;
+}
+</style>
